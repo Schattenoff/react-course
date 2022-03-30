d8f370ff093afede685dda7bf5a48e8c895add37
Schatten <schattenoff@example.com>
2022-03-30 23:36:34 +0300

    load github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import PostForm from "./components/PostForm";
+import PostList from "./components/PostList";
+import './styles/App.css'
 
 function App() {
+
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaScript', desc: 'Javascript - это язык программирования'},
+    {id: 2, title: 'JavaScript 2', desc: 'Javascript - это язык программирования'},
+    {id: 3, title: 'JavaScript 3', desc: 'Javascript - это язык программирования'},
+  ])  
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
+
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostForm create={createPost}/>
+      {posts.length
+      ? <PostList remove={removePost} posts={posts} title="Посты про JS"/>
+      : <h1 style={{textAlign: 'center'}}>Посты не найдены!</h1>
+      }
     </div>
   );
 }
