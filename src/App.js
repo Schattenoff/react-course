004f9c9d4130ed0a536c0727d342517fd0748354
Schatten WORK-PC <schattenwork@example.com>
2022-03-31 15:02:19 +0300

    add functional

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
+import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyModal from "./components/UI/MyModal/MyModal";
 import './styles/App.css'
 
 function App() {
-
   const [posts, setPosts] = useState([
-    {id: 1, title: 'JavaScript', desc: 'Javascript - это язык программирования'},
-    {id: 2, title: 'JavaScript 2', desc: 'Javascript - это язык программирования'},
-    {id: 3, title: 'JavaScript 3', desc: 'Javascript - это язык программирования'},
+    {id: 1, title: 'Kolya', desc: 'prikol 1'},
+    {id: 2, title: 'Tanya', desc: 'prikol 2'},
+    {id: 3, title: 'Nastya', desc: 'prikol 3'},
   ])  
+
+  const [filter, setFilter] = useState({sort: '', query: ''})
+
+  const [modal, setModal] = useState(false);
+
+  const sortedPosts = useMemo(() => {
+    if(filter.sort) {
+      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+    }
+    return posts;
+  }, [filter.sort, posts]) 
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post =>  post.title.toLowerCase(). includes(filter.query))
+  }, [filter.query, sortedPosts])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false);
   }
 
   const removePost = (post) => {
@@ -21,11 +39,17 @@ function App() {
 
   return (
     <div className="App">
-      <PostForm create={createPost}/>
-      {posts.length
-      ? <PostList remove={removePost} posts={posts} title="Посты про JS"/>
-      : <h1 style={{textAlign: 'center'}}>Посты не найдены!</h1>
-      }
+      <MyButton onClick={() => setModal(true)} >
+        Создать пост
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost}/>
+      </MyModal>
+      <PostFilter 
+      filter={filter}
+      setFilter={setFilter}
+       />
+       <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
     </div>
   );
 }
